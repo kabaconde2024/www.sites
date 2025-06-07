562875fe37e9697411f2693c96835ff81c69a17c
kabaconde2024 <kabaconde5259@gmail.com>
2025-06-07 16:44:27 +0100

    Initial commit

--- Back_end/routes/routesAuth.js
+++ Back_end/routes/routesAuth.js
@@ -6,5 +6,14 @@ const { getUtilisateurs } = require('../controllers/controleurAuth');
 router.post('/inscription', controleurAuth.inscription);
 router.post('/connexion', controleurAuth.connexion);
 router.get('/utilisateurs', getUtilisateurs);
-
+router.get('/', (req, res) => {
+  res.json({
+    message: "Bienvenue dans l'API Auth",
+    endpoints: {
+      inscription: "POST /inscription",
+      connexion: "POST /connexion",
+      liste_utilisateurs: "GET /utilisateurs"
+    }
+  });
+});
 module.exports = router;
